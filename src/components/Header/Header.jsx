5b253152ec34cee71b6f0162c6e581d159557e76
emdadhemon <emdadhossainemon@gmail.com>
2023-04-08 22:34:27 +0600

    retrive product from local storage

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import './Header.css'
 import logo from '../../images/Logo.svg'
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
         <nav className='header'>
-            <img src= {logo} alt="" />
+            <Link><img src= {logo} alt="" /></Link>
             <div>
-                <a href="">Order</a>
-                <a href="">Order Review</a>
-                <a href="">Manage Inventory</a>
-                <a href="">Login</a>
+                <Link to="/shop">Shop</Link>
+                <Link to="/order">Order</Link>
+                <Link to="/order_review">Order Review</Link>
+                <Link to="/manage_inventory">Manage Inventory</Link>
+                <Link to="/login">Login</Link>
             </div>
         </nav>
     );
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,36 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home/Home';
+import Shop from './components/Shop/Shop';
+import Login from './components/Login/Login';
 
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element : <Home></Home>,
+    children : [
+      {
+        path : "/",
+        element : <Shop></Shop>
+      },
+      {
+        path : "/shop",
+        element : <Shop></Shop>
+      },
+      {
+        path : "login",
+        element : <Login></Login>
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routes}></RouterProvider>
   </React.StrictMode>,
 )
